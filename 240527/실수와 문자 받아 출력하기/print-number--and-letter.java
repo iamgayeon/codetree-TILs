880cf4854e4bd0ac8aef9d6fa572098c0ff67c3e
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 19:29:04 +0900

    Title: 실수와 문자 받아 출력하기, Time: 179ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240527/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String c = sc.next();
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        System.out.println(c);
+        System.out.printf("%.2f\n", a);
+        System.out.printf("%.2f\n", b);
+
+    }
+}
