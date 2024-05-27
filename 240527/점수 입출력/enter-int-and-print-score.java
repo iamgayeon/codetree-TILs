56985e965b7335ce19f54a479cf6a473d0f3cf17
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 17:18:21 +0900

    Title: 점수 입출력, Time: 227ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/점수 입출력/enter-int-and-print-score.java
+++ 240527/점수 입출력/enter-int-and-print-score.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.printf("Your score is %d point.", %d);
+        System.out.printf("Your score is %d point.", a);
     }
 }
