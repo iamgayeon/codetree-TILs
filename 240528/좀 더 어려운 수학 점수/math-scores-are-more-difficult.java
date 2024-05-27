afb286962623be3da1194435e74df87e668778d8
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 00:32:30 +0900

    Title: 좀 더 어려운 수학 점수, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int b1 = sc.nextInt();
+        int b2 = sc.nextInt();
+
+        if(a1 > b1){
+            System.out.print("A");
+        }
+        else if(a1 < b1){
+            System.out.print("B");
+        }
+        else{
+            if(a2 > b2){
+                System.out.print("A");
+            }
+            else if (a2 < b2){
+                System.out.print("B");
+            }
+        }
+
+    }
+}
