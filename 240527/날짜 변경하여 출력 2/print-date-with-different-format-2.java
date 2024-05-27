50c3cc950decbbbb1929540e9e8648a363586133
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 19:38:44 +0900

    Title: 날짜 변경하여 출력 2, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/날짜 변경하여 출력 2/print-date-with-different-format-2.java
+++ 240527/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -6,6 +6,6 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        System.out.print(c+"."+b+"."+a);
+        System.out.print(c+"."+a+"."+b);
     }
 }
