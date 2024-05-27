c0e09bb3c2b98d32ef6bbdf13f76c85bd2d620d9
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 00:40:39 +0900

    Title: 두 사람, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240528/두 사람/two-person.java
+++ 240528/두 사람/two-person.java
@@ -3,19 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a1 = sc.nextInt();
-        String a = sc.next().charAt(0);
+        char a = sc.next().charAt(0);
         int b1 = sc.nextInt();
-        String b = sc.next().charAt(0);
-        if(a1 >= 19 || b1>= 19){
-            if(a == 'M' || b == 'M'){
+        char b = sc.next().charAt(0);
+        if((a1 >= 19 &&  a == 'M') || (b1>= 19 && b == 'M')){
                 System.out.print(1);
             }
-            else{
+        else{
                 System.out.print(0);
             }
-        }
-        else{
-            System.out.print(0);
-        }
+
     }
 }
