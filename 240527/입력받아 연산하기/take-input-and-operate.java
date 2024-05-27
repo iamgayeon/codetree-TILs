5ba3df24c9d3a3b52ead453cb80a16b4d01399c3
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 20:11:20 +0900

    Title: 입력받아 연산하기, Time: 198ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/입력받아 연산하기/take-input-and-operate.java
+++ 240527/입력받아 연산하기/take-input-and-operate.java
@@ -5,7 +5,7 @@ public class Main {
         int a = sc.nextInt();
         int  b = sc.nextInt();
         a = a+87;
-        b /=10;
+        b = b % 10;
         System.out.println(a);
         System.out.println(b);
     }
