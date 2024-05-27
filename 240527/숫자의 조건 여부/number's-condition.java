9f34566746f9047811f46e00406a7549126538f7
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 20:36:15 +0900

    Title: 숫자의 조건 여부, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/숫자의 조건 여부/number's-condition.java
+++ 240527/숫자의 조건 여부/number's-condition.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a>113){
+        if(a>=113){
             System.out.print(1);
         }
         else{
