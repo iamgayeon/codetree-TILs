1285ee06d11511c10a604afb2a64129ef4539909
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 20:02:51 +0900

    Title: 두수의 곱과 몫, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 240527/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.NextInt();
-        int b = sc.NextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.println(a+" * "+b+" = " + (a*b));
         System.out.println(a+" / "+b+" = " + (a/b));
