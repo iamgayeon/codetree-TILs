0792be0237bdb07fcdad17d92e90ed6ccc7f1677
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 19:54:34 +0900

    Title: 간단한 사칙연산, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/간단한 사칙연산/simple-arithmetic-operation.java
+++ 240527/간단한 사칙연산/simple-arithmetic-operation.java
@@ -6,7 +6,7 @@ public class Main {
         int b = sc.nextInt();
         System.out.println(a+b);
         System.out.println(a-b);
-        System.out.println(a*b);
+        System.out.println(a/b);
         System.out.println(a%b);
         }
 }
