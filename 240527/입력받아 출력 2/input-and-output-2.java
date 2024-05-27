dcbee28af75c61055d4d0bbe5ea993e0d6aac1ba
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 19:41:30 +0900

    Title: 입력받아 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240527/입력받아 출력 2/input-and-output-2.java
+++ 240527/입력받아 출력 2/input-and-output-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.delimeter("-");
+        sc.useDelimeter("-");
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.print(a);
