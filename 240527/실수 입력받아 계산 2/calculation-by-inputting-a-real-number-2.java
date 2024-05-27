200bcf5364961824a52a37102db75de8df17e3fe
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 17:28:35 +0900

    Title: 실수 입력받아 계산 2, Time: 177ms, Memory: 11MB, Status: Passed - CodeTree

--- 240527/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
+++ 240527/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
-        System.out.printf("%.2f", a+1.5)
+        System.out.printf("%.2f", a+1.5);
         
     }
 }
