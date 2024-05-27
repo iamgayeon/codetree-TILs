24fb8e9d9d0c1577b734d60fc799e66591844915
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 20:10:29 +0900

    Title: 입력받아 연산하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240527/입력받아 연산하기/take-input-and-operate.java
+++ 240527/입력받아 연산하기/take-input-and-operate.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int  b = sc.nextInt();
         a = a+87;
         b /=10;
         System.out.println(a);
