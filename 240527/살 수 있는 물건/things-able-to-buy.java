71256adb7ba7315ad2bb1f16baaba3ed209d797b
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 21:05:48 +0900

    Title: 살 수 있는 물건, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/살 수 있는 물건/things-able-to-buy.java
+++ 240527/살 수 있는 물건/things-able-to-buy.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if(a == 3000){
+        if(a >= 3000){
             System.out.print("book");
         }
-        else if(a == 1000){
+        else if(a >= 1000){
             System.out.print("mask");
         }
         else{
