9b53539cadcac9e3039f71448caeeda92834192d
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 00:38:24 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240528/두 사람/two-person.java
+++ 240528/두 사람/two-person.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a1 = sc.nextInt();
-        char a = sc.next().charAt(0);
+        String a = sc.next().charAt(0);
         int b1 = sc.nextInt();
-        char b = sc.next().charAt(0);
+        String b = sc.next().charAt(0);
         if(a1 >= 19 || b1>= 19){
             if(a == 'M' || b == 'M'){
                 System.out.print(1);
