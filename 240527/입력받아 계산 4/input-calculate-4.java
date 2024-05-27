7fbc74226ef1011af5f6639ad95b3353729646b0
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-27 17:21:36 +0900

    Title: 입력받아 계산 4, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/입력받아 계산 4/input-calculate-4.java
+++ 240527/입력받아 계산 4/input-calculate-4.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.In);
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.print(a*2);
         
