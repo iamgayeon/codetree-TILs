77fca1a90d3353b5cd14ce3050de5d71feca700e
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 10:57:58 +0900

    Title: 3의 배수 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240528/3의 배수 출력하기/print-multiple-of-number-three.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 3;
+        int n = sc.nextInt();
+        while(i<=n){
+            System.out.print(n +" ");
+            i+=3;
+        }
+    }
+}
