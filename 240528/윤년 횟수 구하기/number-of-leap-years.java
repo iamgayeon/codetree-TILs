8920030f720ac323ecac7d36399d28a1c2c8d6fc
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 14:59:54 +0900

    Title: 윤년 횟수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240528/윤년 횟수 구하기/number-of-leap-years.java
+++ 240528/윤년 횟수 구하기/number-of-leap-years.java
@@ -6,10 +6,12 @@ public class Main {
         int cnt = 0;
         for(int i=1;i<n;i++){
             if(i%4==0){
-                cnt++;}
-            else if(i%100==0 && i%400!=0){
+                if(i%100==0 && i%400!=0){
                 cnt+=0;
-            }
+                }
+                else{
+                    cnt++;
+                }
             else{
                 cnt +=0;
             }
