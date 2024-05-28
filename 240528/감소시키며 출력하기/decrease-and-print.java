3717fd17141ad14efc968d4f8f57b587721939d1
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 10:36:40 +0900

    Title: 감소시키며 출력하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240528/감소시키며 출력하기/decrease-and-print.java
+++ 240528/감소시키며 출력하기/decrease-and-print.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        for(int i=17;i<=5;i-=1){
+        for(int i=17;i>=5;i-=1){
             System.out.print(i + " ");
         }
     }
