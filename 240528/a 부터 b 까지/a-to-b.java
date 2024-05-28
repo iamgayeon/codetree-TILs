d2a87f15d5300f80f1606afb06e4efb8f1f64c55
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 13:53:10 +0900

    Title: a 부터 b 까지, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240528/a 부터 b 까지/a-to-b.java
+++ 240528/a 부터 b 까지/a-to-b.java
@@ -4,15 +4,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.print(a + " ");
+
         for(int i=a;i<b;){
+            System.out.print(i + " ");
             if(i%2==1){
                 i *= 2;
-                System.out.print(i + " ");
             }
             else{
                 i+=3;
-                System.out.print(i + " ");
+               
             }
         }
 
