f2ce2316e0fbe92cf2d33a4dab7c60fab38be8e2
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 11:23:55 +0900

    Title: 문자 출력하기, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240528/문자 출력하기/print-text.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 8; i++){
+            System.out.print("G");
+        }
+        
+    }
+}
