3b5d636d413e6c464e3d4f43899e811b0488f3b7
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 15:58:42 +0900

    Title: 홀수만 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240528/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int s = sc.next();
+            if((s % 2 == 1) && (s % 3 == 0)){
+                sum += i;
+            }
+        }
+        System.out.print(sum)
+    }
+}
