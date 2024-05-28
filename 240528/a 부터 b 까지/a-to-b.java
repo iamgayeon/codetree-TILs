13dcac030bd033d0feb2d34a08e804673f142980
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-28 13:40:38 +0900

    Title: a 부터 b 까지, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240528/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a + " ");
+        for(int i=a;i<b;){
+            if(i%2==1){
+                i *= 2;
+                System.out.print(i + " ");
+            }
+            else{
+                i+=3;
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
