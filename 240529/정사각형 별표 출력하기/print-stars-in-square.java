e83ee7ee87e56c278b8a861804d699a1caafa3a5
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 18:58:11 +0900

    Title: 정사각형 별표 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/정사각형 별표 출력하기/print-stars-in-square.java
+++ 240529/정사각형 별표 출력하기/print-stars-in-square.java
@@ -8,7 +8,7 @@ public class Main {
             for(int j=0;j<n;j++){
                 System.out.print("*");
             }
-            System.out.print();
+            System.out.println();
         }
 
 
