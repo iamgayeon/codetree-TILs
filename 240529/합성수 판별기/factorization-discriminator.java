64b4f66f36413655d340eadac76983934d179437
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 17:41:50 +0900

    Title: 합성수 판별기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240529/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        int n =sc.nextInt();
+        for(int i =1;i<=n;i++){
+            if(n%i==0){
+                cnt += 1;
+            }
+        }
+
+        if(cnt>=1){
+            System.out.print("C");
+        }
+        else{
+            System.out.print("N");
+        }
+
+    }
+}
