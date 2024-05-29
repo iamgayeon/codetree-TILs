567a756fd59500d890263d0c3d0dcc40fa168153
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 16:48:34 +0900

    Title: 몫 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240529/몫 출력하기/print-share.java
+++ 240529/몫 출력하기/print-share.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt=3;
+        int cnt=4;
         while(cnt!=0){
             --cnt;
             int n = sc.nextInt();
