cc38a97fd8ffe7f6931a45fe2357765b819fafc6
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 16:57:49 +0900

    Title: 몫 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/몫 출력하기/print-share.java
+++ 240529/몫 출력하기/print-share.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt=0;
-        while(true){
-            cnt+=1;
+        int s=0;
+        while(s!=3){
             int n = sc.nextInt();
-            if (cnt >4){
-                break;
-            }
             if(n%2==1){
-
+                continue;
             }
             else{
-                System.out.println(n/2);
+                n = n/2;
+                System.out.println(n);
             }
+            s=s+1;
+            
         }
 
     }
