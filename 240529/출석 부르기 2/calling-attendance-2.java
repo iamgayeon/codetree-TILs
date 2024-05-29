a3639632c6245a0d50b1baf0e9854aff0c1bbd19
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 16:42:27 +0900

    Title: 출석 부르기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240529/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            int n = sc.nextInt();
+
+            while(true){
+                if(n == 1){
+                    System.out.print("John");
+                }
+                else if(n == 2){
+                    System.out.print("John");
+                }
+                else if(n == 3){
+                    System.out.print("John");
+                }
+                else if(n == 4){
+                    System.out.print("John");
+                }
+                else{
+                    System.out.print("Vacancy");
+                    break;
+                }
+        }
+
+    }
+}
