5f6492ba18749cfce192fcd3fef77ca8b3c0c9c5
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 17:29:44 +0900

    Title: 2의 거듭제곱수, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum =1;
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+                sum = sum * 2;
+                if(sum == n){
+                    System.out.print(i);
+                    break;
+                }
+            }
+
+
+    }
+}
