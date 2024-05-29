2dcb74ff6a7eb4a6fd07bdd8756ca466d8a81df6
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-29 19:19:25 +0900

    Title: 별표 출력하기 7, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/별표 출력하기 7/print-star-7.java
+++ 240529/별표 출력하기 7/print-star-7.java
@@ -5,7 +5,7 @@ public class Main {
         int n = sc.nextInt();
 
         for(int i=0;i<n;i++){
-            for(int j=0;j>=i;j++){
+            for(int j=0;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
