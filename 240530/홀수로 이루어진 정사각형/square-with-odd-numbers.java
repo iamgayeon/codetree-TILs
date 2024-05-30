6cf3401bd8a24ea63932a85f1014ce8812a9ac78
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-05-30 17:09:47 +0900

    Title: 홀수로 이루어진 정사각형, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/홀수로 이루어진 정사각형/square-with-odd-numbers.java
+++ 240530/홀수로 이루어진 정사각형/square-with-odd-numbers.java
@@ -5,13 +5,14 @@ public class Main {
 
         int n = sc.nextInt();
         
-        for(int i = 0;i<n*2;i+=2){
-            
+        int num = 9;
+        for(int i=1;i<=n;i++){
    
-            for(int j=11;j<n*2-1+11;j+=2){
-           
-                System.out.print(i+j + " ");
+            for(int j=1;j<=n;j++ ){
+                System.out.print(num+j*2 + " ");
+                
             }
+            num +=2;
             System.out.println();
         }
     }
