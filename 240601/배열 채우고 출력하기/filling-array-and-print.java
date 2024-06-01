8f94a2aa4191ef8addb36b8484139814a1d612b7
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 13:17:25 +0900

    Title: 배열 채우고 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240601/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[10];
+
+        for(int i=0;i<10;i++){
+            arr[i]= sc.next().charAt(0);
+        }
+
+        for(int i=9;i>=0;i--){
+            System.out.print((char)(arr[i]));
+        }
+        
+    }
+}
