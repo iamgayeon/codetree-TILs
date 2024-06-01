4b22df82688ba364cadafeb859fc9dbca7918e8f
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 16:28:27 +0900

    Title: 점수대 파악하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240601/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[10];
+
+        while(true){
+            int num = sc.nextInt();
+            if(num == 0){
+                break;
+            }
+            int i = num /10;
+            arr[i-1] += 1;
+        }
+        int num = 100;
+        for(int i =9;0<=i;i--){
+            System.out.println(num + " - " + arr[i]);
+            num -=10;
+        }
+
+        
+    }
+}
