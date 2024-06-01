f2da9279aa0086b659dd7bfb2bcd752b99a21045
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 10:31:55 +0900

    Title: 3n + 1 수열 m번, Time: 161ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240601/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 240601/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -8,7 +8,11 @@ public class Main {
         
         for(int i =1;i<=num;i++){
             int n = sc.nextInt();
-            while(n!=1){
+            while(true){
+
+                if(n==1){
+                    break;
+                }
                 if(n%2==0){
                     n= n/2;
                     cnt+=1;
