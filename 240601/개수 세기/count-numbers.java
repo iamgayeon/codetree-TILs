538909e44f35571d966e513e240ca163d9da2bbd
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 17:29:58 +0900

    Title: 개수 세기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240601/개수 세기/count-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] word = new int[n];
+        int count =0;
+
+        for(int i=0;i<n;i++){
+            word[i] = sc.nextInt();
+            if(word[i] == 3){
+                count +=1;
+            }
+        }
+
+        System.out.print(count);
+
+        
+    }
+}
