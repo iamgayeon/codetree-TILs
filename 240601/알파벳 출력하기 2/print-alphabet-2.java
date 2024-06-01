602ff20d945284043cfe14fb6e96a2677e212362
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 10:05:09 +0900

    Title: 알파벳 출력하기 2, Time: 3015ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 240601/알파벳 출력하기 2/print-alphabet-2.java
+++ 240601/알파벳 출력하기 2/print-alphabet-2.java
@@ -15,7 +15,7 @@ public class Main {
 
 
 
-            for(int j=n;j>0;j--){
+            for(int j=0;j<n-i;j--){
                 System.out.print((char)ch + " ");
 
                 if(ch == 'Z'){
