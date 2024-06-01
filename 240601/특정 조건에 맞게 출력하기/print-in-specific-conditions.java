8392fcd44f396b461594481d0aa9a4a7ea2ba681
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 16:02:02 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240601/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 240601/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -3,16 +3,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[] = new int[10];
+        int arr[] = new int[100];
+        int cnt = 0;
 
-        for(int i=0;i<10;i++){
+        for(int i=0;i<100;i++){
             arr[i] = sc.nextInt();
-        }
-
-        for(int i=0;i<10;i++){
+            cnt+=1;
             if(arr[i] == 0){
                 break;
             }
+        
+        }
+
+        for(int i=0;i<cnt-1;i++){
             if(arr[i] %2==1){
                 arr[i] = arr[i]+3;
                 System.out.print(arr[i] + " ");
