37a475c6d872b64a22e5f199d352607484e172a6
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-01 12:52:05 +0900

    Title: 완전수, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240601/완전수/perfect-number.java
+++ 240601/완전수/perfect-number.java
@@ -11,7 +11,7 @@ public class Main {
         for(int i=start;i<=end;i++){
             int sum =0;
 
-            for(int j=1;j<i/2;j++){
+            for(int j=1;j<=i/2;j++){
                 if(i%j==0){
                     sum += j;                
                 }
