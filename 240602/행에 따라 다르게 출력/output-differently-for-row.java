1d7d7b962a8f86127e089754a1cfd25ffc97be92
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-02 15:52:56 +0900

    Title: 행에 따라 다르게 출력, Time: 243ms, Memory: 12MB, Status: Passed - CodeTree

--- 240602/행에 따라 다르게 출력/output-differently-for-row.java
+++ 240602/행에 따라 다르게 출력/output-differently-for-row.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -6,30 +7,25 @@ public class Main {
         int n = sc.nextInt();
 
         int num = 1;
-        int num2 = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i%2==0){
+        int num2;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                // Even row
+                for (int j = 0; j < n; j++) {
                     System.out.print(num + " ");
-                    num +=1;
+                    num += 1;
                 }
-                
-                else{
-                        System.out.print(num2 + " ");
-                        num2 +=2;
+            } else {
+                // Odd row
+                num2 = num + 1;
+                for (int j = 0; j < n; j++) {
+                    System.out.print(num2 + " ");
+                    num2 += 2;
                 }
-                
+                num = num2 - 1; // Update num to the last printed value in the odd row
             }
             System.out.println();
-            if(i%2==0){
-                num2 = num + 1;
-                num = num * 2 + 2;
-                
-            }
-            else{
-                 num2 = num +2;
-            }
-                
         }
     }
 }
