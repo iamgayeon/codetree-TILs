149f6427b304b238ebd787b5ca78ca101d22a285
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-02 17:22:45 +0900

    Title: 숫자 직사각형, Time: 213ms, Memory: 12MB, Status: Passed - CodeTree

--- 240602/숫자 직사각형/rectangle-of-numbers.java
+++ 240602/숫자 직사각형/rectangle-of-numbers.java
@@ -15,6 +15,12 @@ public class Main {
                 num +=1;
             }
         }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
+        }
 
         
     }
