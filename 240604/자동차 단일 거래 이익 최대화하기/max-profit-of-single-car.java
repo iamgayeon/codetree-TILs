5f53b51e8407434ba8b5285a56c96812daba3875
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-04 14:08:46 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 218ms, Memory: 13MB, Status: Passed - CodeTree

--- 240604/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240604/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n  = sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
         int gap = -1;
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for(int i=0; i <arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(i == j){
-                    continue;
-                }
-                int tempgap = arr[i]- arr[j];
-                if(tempgap < 0){
-                    tempgap = 0;
-                }
-                if(gap < tempgap && j < i){
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int tempgap = arr[j] - arr[i];
+                if (gap < tempgap) {
                     gap = tempgap;
                 }
             }
+        }
+
+        if (gap < 0) {
+            gap = 0;
         }
         System.out.print(gap);
     }
