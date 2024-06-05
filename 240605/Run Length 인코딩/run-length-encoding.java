35fc8ec016a2cbc4db88fec40c413e27daa73dab
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-05 17:35:54 +0900

    Title: Run Length 인코딩, Time: 149ms, Memory: 11MB, Status: Passed - CodeTree

--- 240605/Run Length 인코딩/run-length-encoding.java
+++ 240605/Run Length 인코딩/run-length-encoding.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        StringBuilder encodedString = new StringBuilder();
 
-        // 첫 번째 문자와 카운터 초기화
+        String run = "";
+
         char c = s.charAt(0);
-        int count = 1;
+        int count =1;
 
-        // 문자열 순회
-        for (int i = 1; i < s.length(); i++) {
-            if (c == s.charAt(i)) {
-                count++;
-            } else {
-                // 현재 문자와 카운트를 결과 문자열에 추가
-                encodedString.append(c).append(count);
-                // 새로운 문자와 카운터 초기화
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) == c){
+                count +=1;
+            }
+            else{
+                run += c;
+                run += Integer.toString(count);
+
                 c = s.charAt(i);
-                count = 1;
+                count =1;
+
             }
         }
+        run += c;
+        run += Integer.toString(count);
         
-        // 마지막 문자와 카운트를 결과 문자열에 추가
-        encodedString.append(c).append(count);
-
-       System.out.println(encodedString.length());
-        System.out.println(encodedString);
-    
+        System.out.println(run.length());
+        System.out.print(run);
+        
     }
 }
