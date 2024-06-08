3475f07e8a2bf913207ce5e90c9852919e775d8e
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-09 02:22:17 +0900

    Title: 문자열 계속 지우기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240609/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String b = sc.next();
+        sc.close();
+
+        while (true) {
+            int index = s.indexOf(b);
+            if (index == -1) {
+                break;
+            }
+            s = s.substring(0, index) + s.substring(index + b.length());
+        }
+
+        System.out.print(s);
+    }
+}
