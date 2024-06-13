6ef06fce0f2bcb5d81d163ced6113d7e0de4245d
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-13 23:18:24 +0900

    Title: 규칙에 따라 밀기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/규칙에 따라 밀기/push-by-the-rules.java
+++ 240613/규칙에 따라 밀기/push-by-the-rules.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -6,15 +7,13 @@ public class Main {
         String s = sc.next();
         String r = sc.next();
 
-        for(int i=0;i<r.length();i++){
-            if(r.charAt(i) == 'L'){
-                s = s.substring(1,length()-1) + s.substring(0);
-
-            }else{
-                s = s.substring(length()-1) + s.substring(0,length()-1);
+        for (int i = 0; i < r.length(); i++) {
+            if (r.charAt(i) == 'L') {
+                s = s.substring(1) + s.charAt(0);
+            } else {
+                s = s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
             }
         }
         System.out.print(s);
-        
     }
 }
